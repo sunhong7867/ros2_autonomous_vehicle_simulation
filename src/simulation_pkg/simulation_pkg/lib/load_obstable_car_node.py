6a52bcf2234feeddb83507a982e5580c496b746b
Park Hyojin <phyojin0511@gmail.com>
2025-03-10 22:33:35 +0900

    Update: map, traffic, obstacle size and location

--- src/simulation_pkg/simulation_pkg/lib/load_obstable_car_node.py
+++ src/simulation_pkg/simulation_pkg/lib/load_obstable_car_node.py
@@ -3,6 +3,8 @@ from simulation_pkg import basic
 
 def main():
     basic.load_model( "obstacle1", random.choice( basic.model_types) , basic.obstacle_coordinates1 )
+    basic.load_model( "obstacle4", random.choice( basic.model_types) , basic.obstacle_coordinates_1 )
+    basic.load_model( "obstacle5", random.choice( basic.model_types) , basic.obstacle_coordinates_2 )
     
     # For obstacle
     basic.load_model( "obstacle2", random.choice( basic.model_types) , basic.obstacle_coord( basic.obstacle_coordinates2 ) )
